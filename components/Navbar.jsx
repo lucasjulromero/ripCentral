b7ee6615341700db3ecc99f0d9eb64eac40034ed
Lucas Romero <lucas@sibipro.com>
2020-03-01 21:27:02 -0700

    Navbar links and icons included

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import styled from "styled-components";
 import { limeGreen, mintCream } from "../styles/colors";
-import { faCannabis } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCannabis,
+  faShoppingCart,
+  faUserCircle,
+  faSearch,
+  faFileExcel
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const NavbarContainer = styled.div`
   background-color: ${limeGreen};
-  width: 100vw;
-  height: 5em;
-  display: grid;
-  grid-template-columns: 25% 75%;
   position: absolute;
   top: 0;
   left: 0;
+  width: 100%;
+  height: 5em;
+  display: grid;
+  grid-template-columns: 15% 60% 25%;
+  grid-template-rows: 35%;
   box-shadow: 0px 1px 6px black;
 `;
 
@@ -30,6 +37,36 @@ const CannabisContainer = styled.div`
   font-size: 0.6em;
   margin-left: 0.1em;
 `;
+
+const LinkContainer = styled.div`
+  grid-column: 2 / span 2;
+  grid-template-columns: 25% 25% 25% 25%;
+  margin-left: 13em;
+`;
+
+const NavLink = styled.a`
+  margin-left: 2em;
+  font-family: "Nunito sans", sans-serif;
+  color: ${mintCream};
+  font-size: 1.3em;
+  font-weight: 300;
+`;
+
+const IconsContainer = styled.div`
+  color: ${mintCream};
+  grid-column: 3 / span 2;
+  font-size: 1.5em;
+  margin-top: -1.5em;
+  margin-left: 4em;
+`;
+
+const Icons = styled.div`
+  display: flex;
+`;
+
+const Icon = styled.div`
+  margin-left: 2em;
+`;
 const Navbar = () => {
   return (
     <>
@@ -40,6 +77,25 @@ const Navbar = () => {
             <FontAwesomeIcon icon={faCannabis} />
           </CannabisContainer>
         </RipCentralLogo>
+        <LinkContainer>
+          <NavLink>Shop</NavLink>
+          <NavLink>Blog</NavLink>
+          <NavLink>Contact</NavLink>
+          <NavLink>Cannabis Chronicles</NavLink>
+        </LinkContainer>
+        <IconsContainer>
+          <Icons>
+            <Icon>
+              <FontAwesomeIcon icon={faShoppingCart} />
+            </Icon>
+            <Icon>
+              <FontAwesomeIcon icon={faUserCircle} />
+            </Icon>
+            <Icon>
+              <FontAwesomeIcon icon={faSearch} />
+            </Icon>
+          </Icons>
+        </IconsContainer>
       </NavbarContainer>
     </>
   );
--- pages/RipCentralHomePage.jsx
+++ pages/RipCentralHomePage.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import styled from "styled-components";
 import RipCentralHomePageHeader from "../components/RipCentralHomePageHeader.jsx";
 import Navbar from "../components/Navbar";
+
+const NavContainer = styled.div`
+  margin-bottom: 8em;
+`;
 
 const RipCentralHomePage = () => {
   return (
     <>
-      <Navbar />
+      <NavContainer>
+        <Navbar />
+      </NavContainer>
       <RipCentralHomePageHeader />
     </>
   );
