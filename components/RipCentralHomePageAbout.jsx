859674509fe3cebd2bf74d6cee6caf358f0e713c
Lucas Romero <lucas@sibipro.com>
2020-03-04 21:04:21 -0700

    added view button to top sellers

--- components/RipCentralHomePageAbout.jsx
+++ components/RipCentralHomePageAbout.jsx
@@ -12,7 +12,7 @@ const topSellers = [
   {
     imgUrl:
       "https://thumbs.nosto.com/quick/magento-grassus/8/10636/18592e3a4d4960598603f5509cd2ee82b7432de55cd354810186f5e839e3bb4fa/A",
-    name: "Bong",
+    name: "Sus Bong",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     price: "$30.00"
@@ -20,7 +20,7 @@ const topSellers = [
   {
     imgUrl:
       "https://www.water-bongs-glass-pipes.com/3965/oil-glass-smoking-pipe-green-marbles.jpg",
-    name: "Piece",
+    name: "Extra piece",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     price: "$5.00"
@@ -28,7 +28,7 @@ const topSellers = [
   {
     imgUrl:
       "https://i1.wp.com/cbdgreenvalley.com/wp-content/uploads/2019/12/grav-upright-pocket-bubbler.jpg?fit=400%2C500&ssl=1",
-    name: "Bubbler",
+    name: "A Bubbler fs",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     price: "$19.00"
@@ -36,7 +36,7 @@ const topSellers = [
   {
     imgUrl:
       "https://www.grasscity.com/media/catalog/product/cache/efe85777930b284290f92120a6605dd3/1088.jpg",
-    name: "One Hitter",
+    name: "One Hit Wonder",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     price: "$2.00"
@@ -44,7 +44,7 @@ const topSellers = [
   {
     imgUrl:
       "https://cdn.shopify.com/s/files/1/2480/0092/products/1_3a3040ee-e945-4d57-8cab-12d7fe97f10c_2000x.jpg?v=1508712142",
-    name: "Grinder",
+    name: "Grinder City All Stars",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     price: "$2.50"
@@ -76,6 +76,7 @@ const TopSellers = styled.h1`
 const TopSellerItemsContainer = styled.div`
   width: 90vw;
   margin-left: 4em;
+  font-weight: 300;
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
 `;
@@ -102,6 +103,25 @@ const Price = styled.p`
   font-size: 1.5em;
   font-family: "Nunito sans", sans-serif;
 `;
+const Button = styled.button`
+  color: white;
+  font-weight: 300;
+  background-color: ${limeGreen};
+  background-color: ${limeGreen};
+  background-repeat: no-repeat;
+  border: 1px solid ${limeGreen};
+  cursor: pointer;
+  border-radius: 4px;
+  font-family: "Nunito sans", sans-serif;
+  font-size: 1.2em;
+  margin-top: 1em;
+  margin-left: 9.5em;
+  padding: auto;
+  max-height: 1.5em;
+  :hover {
+    border: 1px solid ${limeGreen};
+  }
+`;
 const RipCentralHomePageAbout = () => {
   return (
     <>
@@ -115,7 +135,10 @@ const RipCentralHomePageAbout = () => {
                   <TopSellerImage src={i.imgUrl} />
                   <Name>{i.name}</Name>
                   <Description>{i.description}</Description>
-                  <Price>{i.price}</Price>
+                  <div style={{ display: "flex" }}>
+                    <Price>{i.price}</Price>
+                    <Button>View</Button>
+                  </div>
                 </div>
               </>
             );
